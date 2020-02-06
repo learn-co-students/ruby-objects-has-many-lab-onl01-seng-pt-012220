6d722064184e625143f62015a532604173c5baf4
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-06 04:14:58 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+  attr_accessor :author, :author_name, :title
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    @author ? @author.name : nil
+  end
+end
