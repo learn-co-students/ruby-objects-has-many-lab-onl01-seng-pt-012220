5dc56761bdb7ab6835bad36c4dac4182342ddb19
Hailey Bongo <haileyebongo@gmail.com>
2020-02-19 20:49:26 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,35 @@
+class Post 
+  attr_accessor :title, :author  
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def title 
+    @title 
+  end
+  
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil 
+    end
+  end
+    
+  
+
+end
+    
+    
---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song 
+  attr_accessor :title, :artist 
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def name 
+    @title 
+  end
+  
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil 
+    end
+  end
+    
+  
+
+end
+    
+    
