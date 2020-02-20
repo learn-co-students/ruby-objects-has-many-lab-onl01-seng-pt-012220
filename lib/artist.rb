6656bac0bb373eac04947b307d8e672227a8df8a
Xavier Perez <xperez007@gmail.com>
2020-02-20 15:06:29 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+require 'pry' 
+
+class Artist  
+  attr_accessor :name, :artist 
+  @@all = []
+end
+
+def initialize()
+  @Prince = Prince
+  @@all << self
+
+def self.all 
+  @@all
+end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :Prince 
+  @@all = []
+end
+
+def initialize(Prince)
+  @Prince = Prince
+  @@all << Prince
+
+def self.all 
+  @@all
+  
+def add_song(Kiss)
+    song.artist = self
+    
+def song
+  song.all.find_all do |songs|
+
+def self.find_by_name(Prince)
+  new.artist = Artist.new(Prince)
+  self.all
+  @@all.find {|Artist| artist.Prince == Prince}
+end
+
+
+
+
+end
+end
+
